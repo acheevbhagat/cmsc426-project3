d87964910b53b84e9417520ad379f212024458ee
Acheev Bhagat <acheevbhagat@hotmail.com>
2018-11-08 16:48:05 -0500

    Add LocalWindowCenter to colorconfidence struct to make shape indexing easier

--- code/initShapeConfidences.m
+++ code/initShapeConfidences.m
@@ -1,4 +1,13 @@
 function ShapeConfidences = initShapeConfidences(LocalWindows, ColorConfidences, WindowWidth, SigmaMin, A, fcutoff, R)
 % INITSHAPECONFIDENCES Initialize shape confidences.  ShapeConfidences is a struct you should define yourself.
 
+    confidences = {}
+     
+    cConf = ColorConfidences.Confidences;
+    cDist = ColorConfidences.Distance;
+    cCenter = ColorConfidences.LocalWindowCenter;
+    
+    for window = 1:(length(LocalWindows))
+    end
+
 end
