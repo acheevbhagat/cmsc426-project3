a3fa8632777cee56b01577710da83fd491873dee
Acheev Bhagat <acheevbhagat@hotmail.com>
2018-11-08 18:55:08 -0500

    Implement shape confidences

--- code/initShapeConfidences.m
+++ code/initShapeConfidences.m
@@ -1,13 +1,34 @@
 function ShapeConfidences = initShapeConfidences(LocalWindows, ColorConfidences, WindowWidth, SigmaMin, A, fcutoff, R)
 % INITSHAPECONFIDENCES Initialize shape confidences.  ShapeConfidences is a struct you should define yourself.
 
-    confidences = {}
+    confidences = {};
      
     cConf = ColorConfidences.Confidences;
     cDist = ColorConfidences.Distance;
     cCenter = ColorConfidences.LocalWindowCenter;
     
     for window = 1:(length(LocalWindows))
+        % Find index wihtin color confidences of the current window
+        idx = find(cCenter==window);
+        
+        % Use index to set up initial values
+        fc = cConf(idx);
+        dx = cDist(idx);
+        
+        sigma = SigmaMin;
+        
+        % Adapt sigma value based on fc
+        if fc > cutoff 
+            addVal = A * (fc - fcutoff).^(R);
+            sigma = sigma + addVal;
+        end
+        
+        fs = 1 - exp((-(dx.^2)) / (sigma.^2));
+        
+        confidences = {confidences fs}
+        
     end
 
+    ShapeConfidences = struct('Confidences', confidences);
+    
 end
