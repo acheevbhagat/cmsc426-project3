ea43b411278abd59c4e10700d15df8ace49d6e52
Vishal Hundal <vishalhundal@Vishals-iMac.home>
2018-11-12 23:35:32 -0500

    flowwarp??
    flowWarp working??

--- code/localFlowWarp.m
+++ code/localFlowWarp.m
@@ -1,7 +1,85 @@
 function [NewLocalWindows] = localFlowWarp(WarpedPrevFrame, CurrentFrame, LocalWindows, Mask, Width)
 % LOCALFLOWWARP Calculate local window movement based on optical flow between frames.
 
-% TODO
+% WarpedPrevFrame = out
+% CurrentFrame = ingImg
+% LocalWindows = windows
+% Mask = 
+% Width = wSize
+% Calculate tform
+opticFlow = opticalFlowFarneback();
+flow = estimateFlow(opticFlow, rgb2gray(WarpedPrevFrame));
+windows = LocalWindows;
+
+for i=1:size(windows,2)
+    window = windows{i};
+    X = round(window.Position(1) - (Width/2));
+    Y = round(window.Position(2) - (Width/2));
+    XX = X + Width;
+    YY = Y + Width;
+    if(XX >= size(CurrentFrame, 2))
+        XX = size(CurrentFrame, 2);
+        X = XX - (Width);
+    end
+    
+    if(YY >= size(CurrentFrame,1))
+       YY = size(CurrentFrame,1);
+       Y = YY - Width;
+    end
+    
+    Vx = flow.Vx;
+    Vy = flow.Vy;
+    Vx = Vx(Y:YY, X:XX);
+    Vx(window) = NaN; %windows.BgMask == 1
+    
+    Vy = Vy(Y:YY, X:XX);
+    Vy(window) = NaN; %window.BgMask == 1
+    
+    avg_Vx = (mean(mean(Vx,2,'omitnan'),1,'omitnan'));
+    avg_Vy = (mean(mean(Vy,2,'omitnan'),1,'omitnan'));
+    
+    
+    %if(isnan(avg_Vx))
+    %    avg_Vx = 0;
+    %end
+    
+    %if(isnan(avg_Vy))
+    %    avg_Vy = 0;
+    %end
+    
+    window.Position = [(window.Position(1) + avg_Vx) ...
+        (window.Position(2) + avg_Vy)];
+    
+    windows{i}.Position = window.Position;    
+end
+
+t = Width/2;
+
+
+for i=1:numel(windows)
+    pos = windows{i};%.Position
+    X = round(pos(1));
+    Y = round(pos(2));
+    if(X + (Width/2) >= size(CurrentFrame,2))
+        X = size(CurrentFrame,1) - (Width/2);
+    end
+    
+    if(Y + (Width/2) >= size(CurrentFrame,2))
+        Y = size(CurrentFrame,1) - (Width/2);
+    end
+    
+    new_img = CurrentFrame(Y-t:Y+t,X-t:X+t,:);
+    
+    windows{i} = new_img;%.Image
+    
+    %F = zeros([Width+1 Width+1]);
+    %F = mask(Y-t:Y+t,X-t:X+t) == 1;
+    %Bg = (F ~= 1);
+    %windows{i}.Fg = F;
+    %windows{i}.Bg = Bg;
+end
+
+NewLocalWindows = windows;
 
 end
 
